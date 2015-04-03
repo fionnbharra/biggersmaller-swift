b33b06fcaba5760b6c1d2a357fcb7940a1681274
Finbar Sheahan <finbar.sheahan@gmail.com>
2015-04-03 20:52:51 +0100

    Initial Commit

---
+++ biggersmaller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  biggersmaller
+//
+//  Created by Finbar Sheahan on 03/04/2015.
+//  Copyright (c) 2015 Finbar Sheahan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
